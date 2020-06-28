3bf18eb23e1ae0991a08ddb0ee231f43845f9473
Joshua Tan <jtanjoshua@gmail.com>
2020-06-28 09:05:11 +0800

    basic search component

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="title">Link</div>
+        <div className="search-bar">
+          <input type="text" className="search-input"/>
+          <button>Search</button>
+        </div>
+
       </header>
+      <div>Homepage stuff will go here</div>
     </div>
   );
 }
