f2a9a14d9d55b236f37fbb522908b6c7df256dc4
Joshua Tan <jtanjoshua@gmail.com>
2020-07-10 21:55:04 +0800

    screens

---
+++ src/screens/SearchResult.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Main/Header";
+
+const SearchResult = (props) => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default SearchResult;
