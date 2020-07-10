83939cac8fbddf85c259ec15892caa6ef79ab021
Joshua Tan <jtanjoshua@gmail.com>
2020-07-10 18:48:20 +0800

    redux setup for listings

---
+++ src/models/listing.js
@@ -0,0 +1,10 @@
+class Listing {
+  constructor(listingId, title, platform, owner) {
+    this.listingId = listingId;
+    this.title = title;
+    this.platform = platform;
+    this.owner = owner;
+  }
+}
+
+export default Listing;
