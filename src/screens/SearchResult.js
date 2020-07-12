4d5c8bd45027d1e9cd267d3e9a28aa8294ba16b6
Richard Ding <richardding1999@gmail.com>
2020-07-12 17:36:31 +0800

    Search function is now case insensitive and works if you have 1 common word with the listing

--- src/screens/SearchResult.js
+++ src/screens/SearchResult.js
@@ -4,32 +4,30 @@ import dummyListings from '../data/dummyData';
 import SearchListing from '../components/SearchResults/SearchListing'
 import Error404 from './Error404';
 
-class searchAlgorithmns{
-  constructor(searchphrase, listing){
-    this.searchphrase = searchphrase.toLowerCase().replace(/[^a-zA-Z0-9]/g, "")
-    this.listing = listing
+function searchAlgorithmn(searchphrase, listing) {
+
+  searchphrase = searchphrase.toLowerCase().replace(/(<([^>]+)>)/ig,"")
+
+  let wordsInListings = listing.title + " " + listing.platform;
+  // currently only searches for words in the title and platform property
+  wordsInListings = wordsInListings.toLowerCase().split(" ");
+
+  if (wordsInListings.includes(searchphrase)){
+    return listing;
   }
+  else return null
 }
 
 const SearchResult = (props) => {
   let query = props.location.search;
   let listingsToDisplay = [];
 
-
   if (query.slice(0,8)=="?phrase="){
-    let searchphrase = query.substring(8)
+    let searchphrase = query.substring(8);
     // searchphrase = what the user typed
 
-    function searchAlgorithmn(listing) {
-      if (
-        searchphrase.toLowerCase() == listing.title.toLowerCase() ||
-        searchphrase.toLowerCase() == listing.platform.toLowerCase()
-      ){
-        listingsToDisplay.push(listing)
-      }
-    }
-
-    dummyListings.map(listing => searchAlgorithmn(listing))
+    listingsToDisplay = dummyListings.map(listing => searchAlgorithmn(searchphrase,listing));
+    listingsToDisplay = listingsToDisplay.filter(listingToDisplay => listingToDisplay !== null );
 
     return (
       <div style= {{width:"100%"}} >
