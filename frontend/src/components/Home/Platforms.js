5a35975c897025441198bce7e28f9608562bb1a7
Joshua Tan <jtanjoshua@gmail.com>
2020-07-17 22:20:22 +0800

    other logos and styling

--- frontend/src/components/Home/Platforms.js
+++ frontend/src/components/Home/Platforms.js
@@ -1,17 +1,22 @@
 import React from "react";
 import "./Platforms.css";
 import "../../constants/styles/Bootstrap.css";
-import { ReactComponent as XboxLogo } from "../../constants/icons/xbox.svg";
+import XboxLogo from "../../constants/icons/xbox.svg";
+import SwitchLogo from "../../constants/icons/switch.svg";
+import PlaystationLogo from "../../constants/icons/playstation.svg";
 
 const Platforms = (props) => {
   return (
     <div className="container">
       <button className="btn btn-light icon-btn">
-        <XboxLogo />
-        Xbox
+        <img src={XboxLogo} height="100" alt="Xbox" />
       </button>
-      <button className="btn btn-light">Playstation</button>
-      <button className="btn btn-light">Nintendo Switch</button>
+      <button className="btn btn-light icon-btn">
+        <img src={PlaystationLogo} height="100" alt="Playstation" />
+      </button>
+      <button className="btn btn-light icon-btn">
+        <img src={SwitchLogo} alt="Switch" height="100" />
+      </button>
     </div>
   );
 };
