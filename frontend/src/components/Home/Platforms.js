83b3474206da6a5b743966c0c2b39cbc72baae33
Joshua Tan <jtanjoshua@gmail.com>
2020-07-17 20:59:25 +0800

    xbox icon

--- frontend/src/components/Home/Platforms.js
+++ frontend/src/components/Home/Platforms.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Platforms.css";
 import "../../constants/styles/Bootstrap.css";
+import { ReactComponent as XboxLogo } from "../../constants/icons/xbox.svg";
+
 const Platforms = (props) => {
   return (
     <div className="container">
-      <button className="btn btn-light">Xbox</button>
+      <button className="btn btn-light icon-btn">
+        <XboxLogo />
+        Xbox
+      </button>
       <button className="btn btn-light">Playstation</button>
       <button className="btn btn-light">Nintendo Switch</button>
     </div>
