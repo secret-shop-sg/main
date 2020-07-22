c852f5ea93c7c7e40fe6d3987db66ec3d7b9339e
marctan2705 <67046774+marctan2705@users.noreply.github.com>
2020-07-22 13:17:12 +0800

    mistakes

--- frontend/src/RoutingTable.js
+++ frontend/src/RoutingTable.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-import SearchResult from "./screens/SearchResult"
-import Error404 from "./screens/Error404"
-import ListingDetails from "./screens/ListingDetails"
-import UserPage from "./screens/UserPage"
-import Main from "./screens/Main"
-
-const RoutingTable = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Main} />
-                <Route path="/search" component={SearchResult} />
-                <Route path="/listing" component={ListingDetails} />
-                <Route path="/user" component={UserPage} />
-                <Route component={Error404} />
-                {/* Routes client to error404 page if path does not match any of the others*/}
-            </Switch>
-        </Router>
-    )
-}
-=======
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -33,19 +8,19 @@ import Main from "./screens/Main";
 import ServerError from "./screens/errors/ServerError";
 
 const RoutingTable = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route path="/search" component={SearchResult} />
-        <Route path="/listing" component={ListingDetails} />
-        <Route exact path="/error/500" component={ServerError} />
-        <Route component={PageNotFound} />
-        {/* Routes client to PageNotFound page if path does not match any of the others*/}
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Main} />
+                <Route path="/search" component={SearchResult} />
+                <Route path="/listing" component={ListingDetails} />
+                <Route exact path="/error/500" component={ServerError} />
+                <Route path="/user" component={UserPage} />
+                <Route component={PageNotFound} />
+                {/* Routes client to PageNotFound page if path does not match any of the others*/}
+            </Switch>
+        </Router>
+    );
 };
->>>>>>> 314a3882ee08f0bdbe47dd67ad8b45e2038855cc
 
 export default RoutingTable;
