4580a96ac3eb840a0aba556edf81501613d947e1
Richard Ding <richardding1999@gmail.com>
2020-07-23 22:09:54 +0800

    ignore backend node_modules

--- backend/routes/search-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const searchController = require("../controllers/search-controller");
-
-const router = express.Router();
-
-router.get("/", searchController.getSearches);
-
-module.exports = router;
