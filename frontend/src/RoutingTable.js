33e8d6d96a5584f7ac7cdf343352a375c038845a
marctan2705 <67046774+marctan2705@users.noreply.github.com>
2020-07-23 23:44:07 +0800

    managed merge errors

--- frontend/src/RoutingTable.js
+++ frontend/src/RoutingTable.js
@@ -1,31 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import SearchResult from "./screens/SearchResult";
 import ClientSideErrorsPage from "./screens/errors/ClientSideErrorsPage";
 import ListingDetails from "./screens/ListingDetails";
 import Main from "./screens/Main";
+import UserPage from "../src/screens/UserPage";
 import ServerError from "./screens/errors/ServerSideErrorPage";
 
 const RoutingTable = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route path="/search" component={SearchResult} />
-        <Route path="/listing" component={ListingDetails} />
-        <Route exact path="/error/500" component={ServerError} />
-        <Route
-          component={ClientSideErrorsPage}
-          err={{
-            message: "The page you are requesting for could not be found",
-            status: 404,
-          }}
-        />
-        {/* Routes client to ClientSideErrorsPage page if path does not match any of the others*/}
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Main} />
+                <Route path="/search" component={SearchResult} />
+                <Route path="/listing" component={ListingDetails} />
+                <Route path="/user" component={UserPage} />
+                <Route exact path="/error/500" component={ServerError} />
+                <Route
+                    component={ClientSideErrorsPage}
+                    err={{
+                        message: "The page you are requesting for could not be found",
+                        status: 404,
+                    }}
+                />
+                {/* Routes client to ClientSideErrorsPage page if path does not match any of the others*/}
+            </Switch>
+        </Router>
+    );
 };
 
 export default RoutingTable;
