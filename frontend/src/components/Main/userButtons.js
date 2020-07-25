29ce91b8d18974fad22f191c8c5e04958cc49ebd
Joshua Tan <jtanjoshua@gmail.com>
2020-07-25 22:27:52 +0800

    folder for user components

--- frontend/src/components/Main/userButtons.js
+++ frontend/src/components/Main/userButtons.js
@@ -3,54 +3,46 @@ import "./Header.css";
 import { FaUserAlt, FaUserPlus } from "react-icons/fa";
 
 function UserButtons(props) {
-    return (
-        <div id="userbuttonholder">
-            <span
-                className="userButtons"
-                id="login"
-                onClick={() => {
-                    if (props.signupForm) {
-                        props.loginFormHandler();
-                        props.signupFormHandler()
-                    }
-                    else {
-                        props.loginFormHandler()
-                    }
-                }
-                }
-            >
-                <span>
-                    <FaUserAlt size={18} />
-                </span>
-                <span> </span>
-                <span>
-                    Login
-            </span>
-            </span>
-            <span
-                id="signup"
-                className="userButtons"
-                onClick={() => {
-                    if (props.loginForm) {
-                        props.loginFormHandler();
-                        props.signupFormHandler()
-                    }
-                    else {
-                        props.signupFormHandler()
-                    }
-                }
-                }
-            >
-                <span>
-                    <FaUserPlus size={23} />
-                </span>
-                <span> </span>
-                <span>
-                    Signup
-            </span>
-            </span>
-        </div>
-    )
+  return (
+    <div id="userbuttonholder">
+      <span
+        className="userButtons"
+        id="login"
+        onClick={() => {
+          if (props.signupForm) {
+            props.loginFormHandler();
+            props.signupFormHandler();
+          } else {
+            props.loginFormHandler();
+          }
+        }}
+      >
+        <span>
+          <FaUserAlt size={18} />
+        </span>
+        <span> </span>
+        <span>Login</span>
+      </span>
+      <span
+        id="signup"
+        className="userButtons"
+        onClick={() => {
+          if (props.loginForm) {
+            props.loginFormHandler();
+            props.signupFormHandler();
+          } else {
+            props.signupFormHandler();
+          }
+        }}
+      >
+        <span>
+          <FaUserPlus size={23} />
+        </span>
+        <span> </span>
+        <span>Signup</span>
+      </span>
+    </div>
+  );
 }
 
-export default UserButtons
+export default UserButtons;
