f130eb1aba6355b37b5d8c061470f76a6c7cb0ca
Joshua Tan <jtanjoshua@gmail.com>
2020-07-25 22:22:26 +0800

    reducers for login and signup form

--- frontend/src/components/Main/test_files/login.js
+++ frontend/src/components/Main/test_files/login.js
@@ -1,11 +1,58 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useReducer } from "react";
 import "../login.css";
 import "../../../constants/styles/Bootstrap.css";
 import { FaUserCircle } from "react-icons/fa";
+
+// reducer for login data
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE":
+      // update field values
+      const updatedValues = {
+        ...state.inputValues,
+        [action.input]: action.value,
+      };
+      // TODO: validities
+
+      // return updated state
+      return {
+        ...state,
+        inputValues: updatedValues,
+      };
+    default:
+      return state;
+  }
+};
+
 function Login(props) {
+  // use reducer for login data
+  const [formState, dispatchForm] = useReducer(formReducer, {
+    inputValues: {
+      username: "",
+      password: "",
+      // TODO: check if handling password securely
+    },
+    // TODO: input validities
+  });
+
+  // input handler for both fields
+  const inputChangeHandler = (inputIdentifier, value) => {
+    // isValid for third parameter
+
+    console.log(value);
+
+    //dispatch to reducer
+    dispatchForm({
+      type: "UPDATE",
+      value: value,
+      input: inputIdentifier,
+      // TODO: add validity here as another value
+    });
+  };
+
   const escFunction = useCallback((event) => {
     if (event.keyCode === 27) {
-      props.loginFormHandler()
+      props.loginFormHandler();
     }
   }, []);
 
@@ -19,32 +66,28 @@ function Login(props) {
   return (
     <div className="wrapper">
       <div id="formContent">
-        <div
-          className="closebutton"
-          onClick={props.closeButtonHandler}
-        >
+        <div className="closebutton" onClick={props.closeButtonHandler}>
           X
         </div>
-        <div
-          style={{ paddingTop: 10, paddingBottom: 10 }}
-          className="fadeIn"
-        >
+        <div style={{ paddingTop: 10, paddingBottom: 10 }} className="fadeIn">
           <FaUserCircle size={61} />
         </div>
         <form>
           <input
             type="text"
-            id="textthing"
-            className="fadeIn input"
+            id="username"
+            className="fadeIn input textthing"
             name="login"
             placeholder="username"
+            onChange={(e) => inputChangeHandler(e.target.id, e.target.value)}
           />
           <input
             type="password"
-            id="textthing"
-            className="fadeIn input"
+            id="password"
+            className="fadeIn input textthing"
             name="login"
             placeholder="password"
+            onChange={(e) => inputChangeHandler(e.target.id, e.target.value)}
           />
           <input type="submit" className="fadeIn input" value="Log In" />
         </form>
--- frontend/src/components/Main/test_files/signup.js
+++ frontend/src/components/Main/test_files/signup.js
@@ -1,11 +1,61 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useReducer } from "react";
 import "../login.css";
 import "../../../constants/styles/Bootstrap.css";
 import { FaUserCircle } from "react-icons/fa";
+
+// reducer for signup data
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE":
+      // update field values
+      const updatedValues = {
+        ...state.inputValues,
+        [action.input]: action.value,
+      };
+      // TODO: validities
+
+      // return updated state
+      return {
+        ...state,
+        inputValues: updatedValues,
+      };
+    default:
+      return state;
+  }
+};
+
 function Signup(props) {
+  // use reducer for signup data
+  const [formState, dispatchForm] = useReducer(formReducer, {
+    inputValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmedPassword: "",
+      // TODO: check if handling password securely
+    },
+    // TODO: input validities
+  });
+
+  // input handler for all fields
+  const inputChangeHandler = (inputIdentifier, value) => {
+    // isValid for third parameter
+
+    // test
+    console.log(value);
+
+    // dispatch to reducer
+    dispatchForm({
+      type: "UPDATE",
+      value: value,
+      input: inputIdentifier,
+      // TODO: add validity here as another value
+    });
+  };
+
   const escFunction = useCallback((event) => {
     if (event.keyCode === 27) {
-      props.signupFormHandler()
+      props.signupFormHandler();
     }
   }, []);
 
@@ -16,13 +66,11 @@ function Signup(props) {
       document.removeEventListener("keydown", escFunction, false);
     };
   }, []);
+
   return (
     <div className="wrapper">
       <div id="formContent">
-        <div
-          className="closebutton"
-          onClick={props.closeButtonHandler}
-        >
+        <div className="closebutton" onClick={props.closeButtonHandler}>
           X
         </div>
         <div
@@ -34,31 +82,35 @@ function Signup(props) {
         <form>
           <input
             type="text"
-            id="textthing"
-            className="fadeIn input"
+            id="username"
+            className="fadeIn input textthing"
             name="login"
             placeholder="username"
+            onChange={(e) => inputChangeHandler(e.target.id, e.target.value)}
           />
           <input
             type="email"
-            id="textthing"
-            className="fadeIn input"
+            id="email"
+            className="fadeIn input textthing"
             name="login"
             placeholder="email"
+            onChange={(e) => inputChangeHandler(e.target.id, e.target.value)}
           />
           <input
             type="password"
-            id="textthing"
-            className="fadeIn input"
+            id="password"
+            className="fadeIn input textthing"
             name="login"
             placeholder="password"
+            onChange={(e) => inputChangeHandler(e.target.id, e.target.value)}
           />
           <input
             type="password"
-            id="textthing"
-            className="fadeIn input"
+            id="passwordConfirmed"
+            className="fadeIn input textthing"
             name="login"
             placeholder="confirm password"
+            onChange={(e) => inputChangeHandler(e.target.id, e.target.value)}
           />
           <input
             type="submit"
