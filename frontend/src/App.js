c4cd40b6d224fdccf07e6986ebd1d3f2089547f1
tanjoshua <jtanjoshua@gmail.com>
2020-08-19 12:07:54 +0800

    removing redux implementation and libraries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,20 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { combineReducers, createStore } from "redux";
 import "./App.css";
 import RoutingTable from "./RoutingTable";
 
-// import reducers
-import userReducer from "./store/reducers/userReducer";
-
-// combine reducers
-const rootReducer = combineReducers({
-  user: userReducer,
-});
-
-// create store
-const store = createStore(rootReducer);
-
 function App() {
   return (
-    <Provider store={store}>
-      <div>
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-          crossOrigin="anonymous"
-        />
-        <div className="App">
-          <RoutingTable />
-        </div>
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+        crossOrigin="anonymous"
+      />
+      <div className="App">
+        <RoutingTable />
       </div>
-    </Provider>
+    </div>
   );
 }
 
